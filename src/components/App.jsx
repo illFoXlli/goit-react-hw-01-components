321b623d7a86ad95a8f95ab56e508db6a8f46085
Denis <fox.ruden@gmail.com>
2022-09-09 01:34:46 +0300

    end day

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,29 +1,31 @@
-import { Profile } from './Profile/Profile';
-// import { Statistics } from './Statistics/Statistics';
+import { Profile } from './Profile';
+import { Statistics } from './Statistics';
 
 import user from '../data/user.json';
+import data from '../data/data.json';
 
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101',
-      }}
-    >
-      <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        followers={user.stats.followers}
-        views={user.stats.views}
-        likes={user.stats.likes}
-      />
-    </div>
-  );
+ return (
+  <div
+   style={{
+    height: '100vh',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontSize: 40,
+    color: '#010101',
+   }}
+  >
+   <Profile
+    username={user.username}
+    tag={user.tag}
+    location={user.location}
+    avatar={user.avatar}
+    followers={user.stats.followers}
+    views={user.stats.views}
+    likes={user.stats.likes}
+   />
+   <Statistics stats={data} />
+  </div>
+ );
 };
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,2 +1,54 @@
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
+
+import style from './Statistics.module.css';
+
+export function Statistics(props) {
+ const { stats } = props;
+ console.log(props);
+ return (
+  <section className={style.card}>
+   <div className={style.cardFront}></div>
+   <div className={style.cardBack}>
+    <h2>Upload stats</h2>
+    <div className={style.socialIcons}>
+     <ul className={style.statList}>
+      {stats.map(({ percentage, label, id }) => {
+       return (
+        <li key={id} className={style.item}>
+         <span className={style.label}> {label}</span>
+         <span className={style.percentage}>{percentage}%</span>
+        </li>
+       );
+      })}
+     </ul>
+    </div>
+   </div>
+  </section>
+ );
+}
+
+//   <section className={style.statistics}>
+//    <h2 className={style.title}> Upload stats </h2>
+//    <ul className={style.statList}>
+//     {stats.map(({ percentage, label, id }) => {
+//      return (
+//       <li key={id} className={style.item}>
+//        <span className={style.label}>{percentage}</span>
+//        <span className={style.percentage}>{label}</span>
+//       </li>
+//      );
+//     })}
+//    </ul>
+//   </section>
+//  );
+// }
+
+Statistics.propTypes = {
+ stats: PropTypes.arrayOf(
+  PropTypes.shape({
+   id: PropTypes.string.isRequired,
+   label: PropTypes.string.isRequired,
+   percentage: PropTypes.number.isRequired,
+  })
+ ),
+};
